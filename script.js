25650be6a3aec64c2fef450a9857c60c681f75db
jjuampaa <162063711+jjuampaa@users.noreply.github.com>
2024-09-15 20:57:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Cuenta atrás
+const countdownDate = new Date("2024-09-16T20:00:00").getTime();
+
+const countdownFunction = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = 
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
